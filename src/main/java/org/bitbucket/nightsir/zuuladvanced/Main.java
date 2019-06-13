c34c23db380e45e79fef0f47e7772607ce173416
nightsir <christian,sami96@gmail.com>
2019-06-13 10:18:55 +0200

    update to Java11+

--- src/main/java/org/bitbucket/nightsir/zuuladvanced/Main.java
+++ src/main/java/org/bitbucket/nightsir/zuuladvanced/Main.java
@@ -1,32 +1,8 @@
 package org.bitbucket.nightsir.zuuladvanced;
 
-import org.bitbucket.nightsir.zuuladvanced.ui.controller.StreamController;
-import org.bitbucket.nightsir.zuuladvanced.ui.controller.WindowController;
-
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-/**
- * Main-class of the application.<br>
- * Has the JavaFX {@link Application#start(Stage)} method to start the first UI.
- * Initialises the {@link StreamController} and starts the JavaFX application
- * with {@link Application#launch(String...)}.
- * 
- * @author Christian Sami
- */
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		WindowController.get().openMainWindow(primaryStage);
-		StreamController.changeSystemError();
-	}
-	
-	/**
-	 * Main method to start the whole application.
-	 * 
-	 * @param args
-	 */
+public class Main {
 	public static void main(String[] args) {
-		launch(args);
+		App.run(args);
 	}
+
 }
